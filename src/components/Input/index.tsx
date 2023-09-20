087c67dc44c591bef1d9ad6920bced30f34f6605
jhony <joao-victorgs@hotmail.com>
2023-09-20 10:58:06 -0300

    fix: Corrige o componente Input e adiciona o componente CustomErrorMessage

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,25 @@
 import { Field } from "formik";
 import { InputHTMLAttributes } from "react";
 import styles from "./styles.module.css";
+import { ErrorMessageType } from "../../types/ErrorMessageType";
+import { CustomErrorMessage } from "../CustomErrorMessage";
 
-export interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+export interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  errors: ErrorMessageType;
+}
 
-export const Input = ({ name, type, placeholder }: FormInputProps) => {
+export const Input = ({ name, type, placeholder, errors }: FormInputProps) => {
   return (
     <div className={styles.inputContent}>
-      <Field
-        className={styles.field}
-        type={type}
-        name={name}
-        placeholder={placeholder}
-      />
+      <div className={styles.input}>
+        <Field
+          className={styles.field}
+          type={type}
+          name={name}
+          placeholder={placeholder}
+        />
+      </div>
+      {errors ? <CustomErrorMessage message={errors} /> : null}
     </div>
   );
 };
