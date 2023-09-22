d2eb59685a90e7ff41590548c874fb6fd87bac25
Mariana <mari.iwaivk@gmail.com>
2023-09-21 22:26:47 -0300

    feat: Implementa forgetPassword Schema

--- src/utils/validationsSchemas.ts
+++ src/utils/validationsSchemas.ts
@@ -9,3 +9,9 @@ export const loginSchema = Yup.object().shape({
     .required(errrorMessages.email.required),
   password: Yup.string().required(errrorMessages.password.required)
 });
+
+export const forgetPasswordSchema = Yup.object().shape({
+  email: Yup.string().test("email", errrorMessages.email.invalid, value => {
+    return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i.test(value || "");
+  })
+});
