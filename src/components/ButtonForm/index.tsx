b2cc244f05ee0ecdc16421d448dd05984c1693ee
Mariana <mari.iwaivk@gmail.com>
2023-09-21 22:16:51 -0300

    feat: Adiciona onClick() ao componente ButtonForm

--- src/components/ButtonForm/index.tsx
+++ src/components/ButtonForm/index.tsx
@@ -3,11 +3,17 @@ import styles from "./styles.module.css";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
+  onClick?(): void;
 }
 
-export const ButtonForm = ({ text, type, disabled }: ButtonProps) => {
+export const ButtonForm = ({ text, type, disabled, onClick }: ButtonProps) => {
   return (
-    <button className={styles.button} type={type} disabled={disabled}>
+    <button
+      className={styles.button}
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+    >
       <article className={styles.text}>{text}</article>
     </button>
   );
