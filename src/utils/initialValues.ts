0c41c46a5e13fc0d29c96fdd5db07ec6d5848a01
Mariana <mari.iwaivk@gmail.com>
2023-09-21 22:25:47 -0300

    feat: Implementa valores iniciais para forgetPassword e resetPassword

--- src/utils/initialValues.ts
+++ src/utils/initialValues.ts
@@ -1,6 +1,18 @@
+import { ForgetPasswordType } from "../types/ForgetPasswordType";
 import { LoginPostType } from "../types/LoginPostType";
+import { ResetPasswordType } from "../types/ResetPasswordType";
 
 export const loginInitialValues: LoginPostType = {
   email: "",
   password: ""
+};
+
+export const forgetPassword: ForgetPasswordType = {
+  email: ""
+};
+
+export const resetPassword: ResetPasswordType = {
+  reset_password_token: "",
+  password: "",
+  password_confirmation: ""
 };
