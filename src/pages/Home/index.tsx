f39c04b5c9c90d3d4d401967a5739c4f77915b9f
Mariana <mari.iwaivk@gmail.com>
2023-09-28 20:54:46 -0300

    feat: Adiciona HomeDishes e link para DishesList

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { HomeSlogan } from "../../components/HomeSlogan";
 import { MainBackground } from "../../components/MainBackground";
+import { HomeDishes } from "../../components/HomeDishes";
 
 export const Home = () => {
   return (
     <MainBackground>
       <HomeSlogan />
+      <HomeDishes />
     </MainBackground>
   );
 };
