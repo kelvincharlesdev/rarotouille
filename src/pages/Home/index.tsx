4e0483995953527a78d7fdaede3c6fe4f42237f9
kelvin <kelvinc1291@hotmail.com>
2023-09-30 17:29:57 -0300

    feat: Implementa montagem de seção pratos próximos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,20 @@
 import { HomeSlogan } from "../../components/HomeSlogan";
 import { MainBackground } from "../../components/MainBackground";
+import styles from "./styles.module.css";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { SectionDishesClose } from "../../components/SectionDishesClose";
 import { HomeDishes } from "../../components/HomeDishes";
 
 export const Home = () => {
   return (
     <MainBackground>
-      <HomeSlogan />
-      <HomeDishes />
+      <div className={styles.teste}>
+        <HomeSlogan />
+
+        <SectionDishesClose />
+
+        <HomeDishes />
+      </div>
     </MainBackground>
   );
 };
