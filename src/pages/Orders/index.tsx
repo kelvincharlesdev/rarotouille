91338dbb3b29581fe36449e04eadbf6be5f3bc6e
jhony <joao-victorgs@hotmail.com>
2023-09-29 23:23:48 -0300

    feat: Implementa a página Orders

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,3 +1,81 @@
+import { useEffect, useState } from "react";
+import { getOrder, getOrders } from "../../service/apiGet";
+import { MainBackground } from "../../components/MainBackground";
+import { OrderResponseType } from "../../types/OrderResponseType";
+import empityBox from "../../assets/images/BoxEmpity.png"
+import styles from "./styles.module.css";
+import { EmpityListImage } from "../../components/EmpityListImage";
+import { OrderListLine } from "../../components/OrderListLine";
+
 export const Orders = () => {
-  return <div>Orders</div>;
+  const [userLastOrders, setUserLastOrders] = useState<OrderResponseType[]>([])
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getUserOrders = async () => {
+    const res = await getOrders();
+    if (res) {
+      if (res.data.data.length === 0) {
+        setIsLoading(false);
+      }
+      if (res.data.data.length > 5) {
+        const lastFive = res.data.data.slice(res.data.data.length - 5);
+        const copyLastOrders = [{} as OrderResponseType];
+        for (let i = 0; i < 5; i++) {
+          const response = await getUniqueOrder(lastFive[i].id);
+          if (response)
+            copyLastOrders.push(response);
+        }
+        setUserLastOrders(copyLastOrders);
+        setIsLoading(false);
+      } else {
+        const lastFive = res.data.data;;
+        const copyLastOrders = [{} as OrderResponseType];
+        for (let i = 0; i < res.data.data.length; i++) {
+          const response = await getUniqueOrder(lastFive[i].id);
+          if (response)
+            copyLastOrders.push(response);
+        }
+        setUserLastOrders(copyLastOrders);
+        setIsLoading(false);
+      }
+    }
+  }
+  const getUniqueOrder = async (order_id: string) => {
+    const res = await getOrder(order_id);
+    if (res) {
+      const response = res.data;
+      return response;
+    }
+  }
+
+  useEffect(() => {
+    getUserOrders();
+  }, [])
+
+  return (
+    <MainBackground>
+      {isLoading ? (
+        <h1>Carregando...</h1>
+      ) : (
+        <div className={styles.orderListContent}>
+          <h1 className={styles.title}>Seus pedidos</h1>
+          {userLastOrders.length !== 0 ? (
+            <div className={styles.ordersList}>
+              {userLastOrders.map((order) => (
+                <OrderListLine order={order} />
+              ))}
+            </div>
+
+          ) : (
+            <div className={styles.empityBoxContent}>
+              <p className={styles.empityBoxTitle}>Sem pedidos realizados</p>
+              <EmpityListImage imageUrl={empityBox} />
+            </div>
+          )}
+        </div>
+      )}
+
+    </MainBackground>
+
+  );
 };
