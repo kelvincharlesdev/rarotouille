55c8189b60d0bced899baa1c3675eb0d48bc336b
kelvin <kelvinc1291@hotmail.com>
2023-10-01 16:48:55 -0300

    feat: Implementa montagem de mapa na page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,17 +4,17 @@ import styles from "./styles.module.css";
 import { SectionDishesClose } from "../../components/SectionDishesClose";
 import { HomeDishes } from "../../components/HomeDishes";
 import { SectionFavoritesCarousel } from "../../components/SectionFavoritesCarousel";
+import { SectionMap } from "../../components/SectionMap";
 
 export const Home = () => {
   return (
     <MainBackground>
       <div className={styles.teste}>
         <HomeSlogan />
-
         <SectionDishesClose />
         <SectionFavoritesCarousel />
-
         <HomeDishes />
+        <SectionMap />
       </div>
     </MainBackground>
   );
