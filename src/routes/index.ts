d399ebff20b3029796f1f387e2b7bbee001c3aca
jhony <joao-victorgs@hotmail.com>
2023-10-01 19:59:18 -0300

    fix: Remove parâmetros das rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ export const routes = {
   home: "/home",
   dishDetails: (id: string = ":id") => `/dishDetails/${id}`,
   dishesList: "/dishesList",
-  profile: (id: string = ":id") => `/profile/${id}`,
-  orders: (id: string = ":id") => `/orders/${id}`,
-  favorites: (id: string = ":id") => `/favorites/${id}`
+  profile: "/profile",
+  orders:"/orders",
+  favorites: "/favorites"
 };
