f941f6af83595305a8d605188c3c33662cf307c9
jhony <joao-victorgs@hotmail.com>
2023-10-02 00:27:04 -0300

    fix: Adiciona estado do ListControlContext na página Favorites

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,65 +1,33 @@
-import { useEffect, useState } from "react";
 import { MainBackground } from "../../components/MainBackground";
 import styles from "./styles.module.css";
-import { getDishes } from "../../service/apiGet";
-import { DishType } from "../../types/DishType";
 import { SmallCard } from "../../components/SmallCard";
-import { useAuthContext } from "../../contexts/AuthContext";
 import clocheEmpity from "../../assets/images/ClocheEmpity.png"
 import { EmpityListImage } from "../../components/EmpityListImage";
+import { useListControlContext } from "../../contexts/ListControlContext";
 
 export const Favorites = () => {
-  const [dishesList, setDishesList] = useState<DishType[]>([]);
-  const [isLoading, setIsLoading] = useState(true)
-  const {user} = useAuthContext();
-
-  const getFavoritesDishesList = async () => {
-    const response = await getDishes();
-    if (response) {
-      const copyFavorites = response.data.data.filter((dish)=> verifyLike(dish));
-      setDishesList(copyFavorites);
-      setIsLoading(false)
-    }
-  };
-
-  const verifyLike = (dish: DishType) => {
-    const verify = dish.likes?.find((like)=> like.voter_id === user?.id);
-    if(verify){
-      
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-  useEffect(() => {
-    getFavoritesDishesList();
-  }, []);
+  const {favoritesDishes} = useListControlContext();
+  console.log(favoritesDishes)
 
 
   return (
     <MainBackground> 
-      {isLoading ? (
-        <h1>Carregando...</h1>
-      ) : ( 
-       
       <section className={styles.contentDishesList}>
         <h1 className={styles.title}>Favoritos</h1>
-        {dishesList.length === 0 ? (
+        {favoritesDishes.length === 0 ? (
           <div className={styles.empityBoxContent}>
             <p className={styles.empityBoxTitle}>Sem pratos favoritos</p>
             <EmpityListImage imageUrl={clocheEmpity}/>
           </div>
         ) : (
         <div className={styles.dishesList}>
-          {dishesList.map(dish => (
+          {favoritesDishes.map(dish => (
             <SmallCard key={dish.id} dish={dish} />
           ))}
         </div>
 
         )}
       </section>
-      )}
     </MainBackground>
   );
 };
