184eeef455425cf313afd82f4dba002ef4b37f8b
kelvin <kelvinc1291@hotmail.com>
2023-10-02 02:12:38 -0300

    fix: Implementa correção no componente AuthImg

--- src/components/AuthImg/index.tsx
+++ src/components/AuthImg/index.tsx
@@ -1,8 +1,10 @@
+import styles from "./styles.module.css";
+
 interface AuthImgProps {
   src: string;
   alt: string;
 }
 
 export const AuthImg = ({ src, alt }: AuthImgProps) => {
-  return <img src={src} alt={alt} />;
+  return <img className={styles.image} src={src} alt={alt} />;
 };
